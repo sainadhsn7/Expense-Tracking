502acb803c5bd7fc1d4e44547b775ed29ca2245e
sainadhsn7 <tatiparthi_2101cs77@iitp.ac.in>
2024-11-21 17:28:22 +0530

    change folder paths

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ import authRoutes from './routes/authRoutes.js';
 import groupRoutes from './routes/groupRoutes.js'
 import userRoutes from './routes/userRoutes.js';
 import expenseRoutes from './routes/expenseRoutes.js';
+import { fileURLToPath } from 'url';
 
+import path from "path";
 dotenv.config();
 
 const app=express();
@@ -16,21 +18,34 @@ connectDB().catch((error)=>{
     process.exit(1);
 });
 
-app.use(cors({
-    origin: "https://expense-sharing-application.vercel.app/"
-}));
+app.use(cors());
+
 
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name of the current module
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/group', groupRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/expenses', expenseRoutes);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
+
 app.use((err, req, res, next)=>{
     console.error(err.stack);
     res.status(500).json({message: 'Something went wrong', error: err.message})
 });
+
+
 
 const PORT=3000;
 
